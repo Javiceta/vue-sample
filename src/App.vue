69fa75c4ee0087ca19179444a603e670b885570e
Javiceta <javigarciaiceta@gmail.com>
2019-04-23 19:25:56 +0100

    add some components

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <fw-preview ref="preview"></fw-preview>
+    <fw-lista></fw-lista>
+  </div>
+</template>
+
+<script>
+import Lista from "/components/Lista";
+import Preview from "/components/Preview";
+export default {
+  name: "App",
+  components: {
+    "fw-lista": Lista,
+    "fw-preview": Preview
+  },
+  data() {
+    return {
+      elements: [
+        {
+          name: String,
+          image: String,
+          url: String,
+          bgcolor: String
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Monsterrat:400,700");
+
+:root {
+  --bgcolor: #ddd;
+  --textcolor: #fff;
+  background: #222;
+}
+
+#app {
+  margin-top: 4em;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Preview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="preview">
+    <h2>{{ title }}</h2>
+    <img v-show="image" :src="image" :alt="title">
+    <a :href="url">{{ url }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FwPreview",
+  data() {
+    return {
+      title: "",
+      image: "",
+      url: ""
+    };
+  },
+  methods: {
+    setElement(e) {
+      this.name = e.name;
+      this.image = e.image;
+      this.url = e.url;
+    }
+  }
+};
+</script>
+
+<style lang="postcss" scoped>
+.preview {
+  border: 3px solid #fff;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
